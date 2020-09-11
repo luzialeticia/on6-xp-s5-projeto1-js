0d884dccf95b8ee4cd0bd0ccbdc32b829768ffc8
luzialeticia <leticialuzia@tutamail.com>
2020-09-11 01:21:44 -0300

    Criando a variável que recebe o readline-sync

--- projeto/index.js
+++ projeto/index.js
@@ -3,6 +3,7 @@ console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
 const db = require('./database')
+const input = require('readline-sync')
 
 //Listar no console uma tabela contendo os produtos em ordem crescente de preço (do menor ao maior).
 console.log('--------------- Listando os produtos em ordem crescente de preço: ---------------');
@@ -14,3 +15,6 @@ products.sort((a, b) => a.preco - b.preco)
 console.table(products);
 
 console.log('***********************************************************************************************************************');
+
+
+//Receber via terminal as entradas de id e quantidade dos produtos a serem adquiridos.
