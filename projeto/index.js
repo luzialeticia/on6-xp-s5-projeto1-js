29b05add7a33964b41ba131722a8a2834c93d560
luzialeticia <leticialuzia@tutamail.com>
2020-09-11 00:24:05 -0300

    Organizando o código, coloquei comentário e console.logs pra separar o código do 1º passo

--- projeto/index.js
+++ projeto/index.js
@@ -4,8 +4,13 @@ console.log('--------------------------------------')
 
 const db = require('./database')
 
+//Listar no console uma tabela contendo os produtos em ordem crescente de preço (do menor ao maior).
+console.log('--------------- Listando os produtos em ordem crescente de preço: ---------------');
+
 const { produtos:products } = db
 
 products.sort((a, b) => a.preco - b.preco)
 
 console.table(products);
+
+console.log('***********************************************************************************************************************');
