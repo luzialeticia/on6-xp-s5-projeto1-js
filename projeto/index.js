3d95dc7ed401ca3c90c8355c4e8cde1e59ca4ecf
luzialeticia <leticialuzia@tutamail.com>
2020-09-11 00:12:38 -0300

    Fazendo o destructuring e o sort pra ordenar

--- projeto/index.js
+++ projeto/index.js
@@ -3,3 +3,9 @@ console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
 const db = require('./database')
+
+const { produtos } = db
+
+produtos.sort((a, b) => a.preco - b.preco)
+
+console.table(produtos);
