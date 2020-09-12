570c0af5b7676414cdb2e59bcc0d1fd340960547
luzialeticia <leticialuzia@tutamail.com>
2020-09-11 23:29:21 -0300

    Tirando as repetições de dentro da função e reorganizando o código

--- projeto/index.js
+++ projeto/index.js
@@ -18,30 +18,31 @@ console.table(products)
 console.log('***********************************************************************************************************************');
 
 
+
+//Pergunta id do produto
 let id = parseInt(input.question("Digite a ID do produto que deseja: "))
 let productChoose = products.find(item => item.id === id)
-let quantity;
-let hasDiscount;
-let discount;
-let shoppingCart = new Array()
-
-//Função que adicionando itens ao carrinho
-const shopping = () => {
-
     while(productChoose === undefined) {
         id = parseInt(input.question("Produto não encontrado. Digite um ID correto: "))
         productChoose = products.find(item => item.id === id)
     }
-    console.log(`${productChoose.nome} adicionado ao carrinho.`);
+console.log(`${productChoose.nome} adicionado ao carrinho.`);
 
-    quantity = parseInt(input.question("Quantos itens deseja adicionar ao carrinho: "))
-
+//Pergunta quantidade
+let quantity = parseInt(input.question("Quantos itens deseja adicionar ao carrinho: "))
     while(quantity <= 0) {
         quantity = parseInt(input.question("Quantidade inválida. Digite um valor maior que 0: "))
     }
+console.log(`${quantity} ${productChoose.nome} adicionados.`);
 
-    console.log(`${quantity} ${productChoose.nome} adicionados.`);
 
+let hasDiscount;
+let discount;
+let shoppingCart = new Array()
+
+
+//Função que adicionando itens ao carrinho
+const addToCart = () => {
     const item = new Object()
     item.name = productChoose.nome
     item.price = productChoose.preco
